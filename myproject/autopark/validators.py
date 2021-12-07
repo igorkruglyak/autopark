6b8b010cc16170a0b4fe29ec6caa4c63c7d12cd2
Igor Kruglyak <igorkruglyak@gmail.com>
2021-12-07 21:29:57 +0200

    create validators.py

---
+++ myproject/autopark/validators.py
@@ -0,0 +1,8 @@
+from django.core.exceptions import ValidationError
+import re
+
+
+def plate_number_validator(value):
+    to_find = re.findall(r'\b[A-Z]{2} \d\d\d\d [A-Z]{2}\b', value)
+    if len(to_find) == 0:
+        raise ValidationError(f'{value} is a wrong plate number')
