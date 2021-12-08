a1c245d2f35f3abcc66b94108b4d30c907bfcfc3
Igor Kruglyak <igorkruglyak@gmail.com>
2021-12-08 21:56:35 +0200

    create services

---
+++ myproject/autopark/services.py
@@ -0,0 +1,52 @@
+"""Services for the autopark app."""
+from .models import Driver, Vehicle
+import datetime
+from rest_framework.views import Response
+
+
+def get_drivers_list(request):
+    if 'created_at__gte' in request.GET:
+        date = get_splited_date(request.GET['created_at__gte'], '-')
+        # Get all drivers which were created gte request.created_at date
+        drivers_list = Driver.objects.filter(created_at__gte=datetime.datetime(date[2],
+                                                                               date[1], date[0]))
+    elif 'created_at__lte' in request.GET:
+        date = get_splited_date(request.GET['created_at__lte'], '-')
+        # Get all drivers which were created lte request.created_at date
+        drivers_list = Driver.objects.filter(created_at__lte=datetime.datetime(date[2],
+                                                                               date[1], date[0]))
+    else:
+        drivers_list = Driver.objects.all()
+    return drivers_list
+
+
+def get_splited_date(date, symbol):
+    """First argument is a date, and a second is a symbol which is used to split."""
+    return [int(x) for x in date.split(symbol)]
+
+
+def get_vehicles(request):
+    if 'with_drivers' in request.GET:
+        if request.GET['with_drivers'] == 'yes':
+            return Vehicle.objects.filter(driver_id__isnull=False)
+        return Vehicle.objects.filter(driver_id__isnull=True)
+    return Vehicle.objects.all()
+
+
+def get_driver_by_id(driver_id):
+    return Driver.objects.filter(id=driver_id)
+
+
+def get_vehicle_by_id(vehicle_id):
+    return Vehicle.objects.filter(id=vehicle_id)
+
+
+def delete_object_by_id(obj, obj_id):
+    if not obj:
+        return Response(f"No object with id={obj_id}")
+    obj.delete()
+    return Response(f"Success: object with id={obj_id} was deleted")
+
+
+def set_driver_in_vehicle(vehicle, driver_id):
+    vehicle.update(driver_id=driver_id)
--- myproject/autopark/views.py
+++ myproject/autopark/views.py
@@ -1,3 +1,113 @@
-from django.shortcuts import render
+"""Views for the autopark app."""
+from rest_framework import generics
+from rest_framework.views import APIView, Response
+from .services import *
+from .serializers import DriversListSerializer, VehicleListSerializer
 
-# Create your views here.
+
+class DriversListView(APIView):
+    """
+    Get all drivers list, or get driver by date if in request we have "created_at__gte" - means get drivers created
+    greater than equal inputed date or "created_at__lte" - means get drivers created less than equal inputed date.
+    """
+    def get(self, request, *args, **kwargs):
+        drivers_list = get_drivers_list(request)
+        if not drivers_list:
+            return Response(f'Error: No drivers yet')
+        drivers_list_serializer = DriversListSerializer(drivers_list, many=True)
+        return Response(drivers_list_serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        driver_create_serializer = DriversListSerializer(request.data, many=True)
+        if driver_create_serializer.is_valid():
+            driver_create_serializer.save()
+            return Response(f"Success {driver_create_serializer.data['first_name']} "
+                            f"{driver_create_serializer.data['last_name']}"
+                            f"was created")
+        return Response(driver_create_serializer.errors, status=201)
+
+
+class DriverByIdListView(APIView):
+    """
+    Get, put, delete  driver by id.
+    """
+    def get(self, request, driver_id):
+        driver = get_driver_by_id(driver_id)
+        if not driver:
+            return Response(f'Error: No driver with id={driver_id}')
+        driver_serializer = DriversListSerializer(driver, many=True)
+        return Response(driver_serializer.data)
+
+    def put(self, request, driver_id):
+        data = request.data
+        driver = get_driver_by_id(driver_id)
+        driver_serializer = DriversListSerializer(data=data, many=True)
+        if driver_serializer.is_valid():
+            driver.first_name = data[0]['first_name']
+            driver.last_name = data[0]['last_name']
+            driver.save()
+            return Response(f"Success driver was updated")
+        return Response(driver_serializer.errors, status=201)
+
+    def delete(self, request, driver_id):
+        driver = get_driver_by_id(driver_id)
+        return delete_object_by_id(driver, driver_id)
+
+
+class VehiclesListView(APIView):
+    """
+    Get all vehicles, or vehicles with/without driver by adding parameters.
+    Post new vehicle.
+    """
+    def get(self, request):
+        vehicles = get_vehicles(request)
+        if not vehicles:
+            return Response('No vehicles yet')
+        vehicles_serializer = VehicleListSerializer(vehicles, many=True)
+        return Response(vehicles_serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        new_vehicle = VehicleListSerializer(data=request.data, many=True)
+        if new_vehicle.is_valid():
+            new_vehicle.save()
+            return Response(f"Success: vehicle {new_vehicle.data[0]['model']} | "
+                            f"{new_vehicle.data[0]['plate_number']} was created")
+        return Response(new_vehicle.errors, status=201)
+
+
+class VehicleCRUDView(APIView):
+    """
+    Get, put, delete vehicle by id.
+    """
+    def get(self, request, vehicle_id):
+        vehicle = get_vehicle_by_id(vehicle_id)
+        if not vehicle:
+            return Response(f'Error: No vehicle with id={vehicle_id}')
+        vehicle_serializer = VehicleListSerializer(vehicle, many=True)
+        return Response(vehicle_serializer.data)
+
+    def put(self, request, vehicle_id):
+        data = request.data
+        vehicle = get_vehicle_by_id(vehicle_id)
+        vehicle.update(make=data[0]['make'], model=data[0]['model'], plate_number=data[0]['plate_number'])
+        return Response(f"Success: vehicle with id={vehicle_id} was updated")
+
+    def delete(self, request, vehicle_id):
+        vehicle = get_vehicle_by_id(vehicle_id)
+        return delete_object_by_id(vehicle, vehicle_id)
+
+
+class VehicleDriverSetView(APIView):
+    """
+    Post to set or unset driver to vehicle by id.
+    """
+    def post(self, request, vehicle_id):
+        vehicle = Vehicle.objects.filter(id=vehicle_id)
+        driver_id = request.data[0]['driver_id']
+        if not vehicle:
+            return Response(f'Error: No vehicle with id={vehicle_id}')
+        driver = Driver.objects.filter(id=driver_id)
+        if not driver and driver_id:
+            return Response(f'Error: No driver with id={driver_id}')
+        set_driver_in_vehicle(vehicle, driver_id)
+        return Response(f'Success: driver with id={driver_id} was set in vehicle with id={vehicle_id}')
